b50a046dbc563e52a58c20e087efd43c38754b69
spencewine <sdgoodwine@gmail.com>
2018-08-22 15:48:58 -0500

    broken version

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+// import {Auth} from 'aws-amplify'
+
+AppRegistry.registerComponent('rnSimpleBlogRedux', () => App);
---
+++ ios/Pods/Target Support Files/Pods-rnSimpleBlogRedux/Pods-rnSimpleBlogRedux-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rnSimpleBlogRedux : NSObject
+@end
+@implementation PodsDummy_Pods_rnSimpleBlogRedux
+@end
